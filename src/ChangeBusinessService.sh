73bec5c872d8a253a463700e3d7b2beebfb8753a
gurumaia <gurumaia@gmail.com>
2014-08-25 14:09:52 -0300

    Clean-up shell script

--- src/ChangeBusinessService.sh
+++ src/ChangeBusinessService.sh
@@ -2,18 +2,19 @@
 
 #set CLASSPATH
 
-MW_HOME=/opt/web/wl/osb11.1.1.7
+MW_HOME=/opt/web/wl/osb11.1.1.6
 OSB_HOME=${MW_HOME}/osb11
+DOMAIN_NAME=DOMAIN_NAME_HERE
 
-classpath=${MW_HOME}/domains/OSB_VIVO360_1_PROD/scripts/teste/bin:${MW_HOME}/modules/com.bea.core.management.jmx_1.4.2.0.jar:${MW_HOME}/wlserver_10.3/server/lib/weblogic.jar:${OSB_HOME}/lib/modules/com.bea.alsb.utils.jar:${OSB_HOME}/lib/modules/com.bea.alsb.security.api.jar:${OSB_HOME}/lib/modules/com.bea.alsb.resources.globalsettings.jar:${OSB_HOME}/lib/sb-kernel-api.jar:${OSB_HOME}/lib/sb-kernel-common.jar:${OSB_HOME}/lib/sb-kernel-resources.jar:${OSB_HOME}/lib/sb-kernel-impl.jar:${OSB_HOME}/lib/sb-transports-main.jar:${OSB_HOME}/modules/com.bea.common.configfwk_1.7.0.0.jar:${OSB_HOME}/modules/com.bea.alsb.statistics_1.4.0.0.jar:${OSB_HOME}/lib/modules/com.bea.alsb.resources.core.jar
+classpath=${MW_HOME}/domains/${DOMAIN_NAME}/scripts/teste/bin:${MW_HOME}/modules/com.bea.core.management.jmx_1.4.2.0.jar:${MW_HOME}/wlserver_10.3/server/lib/weblogic.jar:${OSB_HOME}/lib/modules/com.bea.alsb.utils.jar:${OSB_HOME}/lib/modules/com.bea.alsb.security.api.jar:${OSB_HOME}/lib/modules/com.bea.alsb.resources.globalsettings.jar:${OSB_HOME}/lib/sb-kernel-api.jar:${OSB_HOME}/lib/sb-kernel-common.jar:${OSB_HOME}/lib/sb-kernel-resources.jar:${OSB_HOME}/lib/sb-kernel-impl.jar:${OSB_HOME}/lib/sb-transports-main.jar:${OSB_HOME}/modules/com.bea.common.configfwk_1.7.0.0.jar:${OSB_HOME}/modules/com.bea.alsb.statistics_1.4.0.0.jar:${OSB_HOME}/lib/modules/com.bea.alsb.resources.core.jar
 
 echo $classpath
 
 sessao=bizWM
-host=aidamina
-port=10000
-username=admWLS
-password=0sbL3g@cyPR3@11
+host=hostnamehere
+port=7001
+username=weblogic
+password=welcome1
 workmanager=BusinessServices.WorkManager
 connectionTimeout=5
 operacaoTimeout=300000
